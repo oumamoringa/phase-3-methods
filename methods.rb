c827dc7370b0a74857025be365218bdb77afb3ea
oumarichard <richard.ouma@student.moringaschool.com>
2022-10-17 12:27:45 +0300

    Tests passed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+greet_programmer
+
+
+def greet name
+  puts  "Hello, #{name}!"
+end
+greet("Naureen")
+greet("Jimmy")
+
+
+def greet_with_default (name="programmer")
+  puts "Hello, #{name}!"
+end
+greet_with_default
+
+
+def add (num1, num2)
+  return num1 + num2
+end
+add(2, 4)
+
+def halve num
+  if num == 6
+    return num/2
+  elsif
+   nil
+  end
+end
+halve(8)
